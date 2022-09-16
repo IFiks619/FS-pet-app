c93548bbece525394adcfb58a813d84b42f57794
IFIks619 <74498870+IFiks619@users.noreply.github.com>
2022-09-16 23:06:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fs from 'fs'
 import mongoose from 'mongoose'
 import multer from 'multer'
 import cors from 'cors'
@@ -23,6 +24,9 @@ app.use('/uploads', express.static('uploads'))
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if(!fs.existsSync('uploads')){
+            fs.mkdirSync('uploads')
+        }
         cb(null, 'uploads')
     },
     filename: (_, file, cb) => {
